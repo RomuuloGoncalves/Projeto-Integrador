bda0b40ec085bad29b8325e84b6a3c3f54b7d393
RomuuloGoncalves <romulogoncalves13@gmail.com>
2024-05-02 20:17:48 -0300

    #

---
+++ Arduino/arduino/arduino.ino
@@ -0,0 +1,50 @@
+#include <WiFiClient.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+char ssid[] = "Wokwi-GUEST";
+char password[] = "";
+
+const char* serverAddress = "https://amused-hopelessly-tetra.ngrok-free.app/Projeto-Integrador/System"; 
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Hello, ESP32!");
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+}
+
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    
+    StaticJsonDocument<200> doc;
+    doc["id_sensor"] = 1;
+    doc["valor"] = rand() % 101;
+
+    String jsonString;
+    serializeJson(doc, jsonString);
+    Serial.println(jsonString);
+
+    http.begin(String(serverAddress) + "/views/dados/cadastro.php");
+    http.addHeader("Content-Type", "application/json", "ngrok-skip-browser-warning");
+
+    int httpResponseCode = http.POST(jsonString);
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println(httpResponseCode);
+      Serial.println(response);
+    } else {
+      Serial.print("Erro ao enviar POST: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end();
+  }
+}
